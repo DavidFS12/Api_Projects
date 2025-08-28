98cccb6489d0991d4b8a150622d96946ce780d04
Davico <cchadavid12@gmail.com>
2025-08-28 17:48:56 -0500

    se instalo y condiguro el frontend para el desarrollo del sistema

---
+++ frontend/src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="h-screen flex items-center justify-center bg-gray-100">
+      <h1 className="text-3xl font-bold text-green-600">🚀 Frontend listo</h1>
+    </div>
+  );
+}
+
+export default App;
